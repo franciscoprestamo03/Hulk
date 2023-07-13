fac69d203d2327e6e9e9cc16984fc5777904996c
Francisco Prestamo <prestamo2003@gmail.com>
2023-07-12 22:07:41 -0600

    Add Initial SyntaxType

---
+++ Hulk/Library/Grammar/SyntaxType.cs
@@ -0,0 +1,15 @@
+namespace Hulk.Library.Grammar
+{
+    public enum SyntaxType
+    {
+        NumericalExpression,
+        AdditionToken,
+        SubtractionToken,
+        MultiplicationToken,
+        DivisionToken,
+        OpenParenthesisToken,
+        CloseParenthesisToken,
+        ErrorToken,
+        
+    }
+}
